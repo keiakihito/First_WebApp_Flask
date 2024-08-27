31ed42b73732b47cc89498937e3ad35b1a35924a
KEITA - KATSUMI <keita-katsumi@b98-aruba-authenticated-10-110-151-20.wlan.cpp.edu>
2024-08-27 15:38:39 -0700

    Adding secret key and user passwork id, password dictionalry  in app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, session, request, redirect, url_for
+import os # It is used for making tokens. 
 
 #Make a Flask instance
 app = Flask(__name__)
 
+ # Make a random value with a parameter
+key = os.urandom(21) 
+app.secret_key = key # This will be a secret key to hold user session. 
+
+
 
 #Assuming it is a "main" function
 # "/" indicates URL path
-#Default is localhost:5000/
+#Default is http://127.0.0.1:5000
 @app.route("/")
 def index() -> str:
 	return "Hello World"

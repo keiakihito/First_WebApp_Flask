6744dcca310b78e9068185982812d316f7845ad6
KEITA - KATSUMI <keita-katsumi@KEITAs-MBP.lan>
2024-08-27 18:47:12 -0700

    Adding login route

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, session, request, redirect, url_for
 import os # It is used for making tokens. 
+from typing import Any, Dict
 
 #Make a Flask instance
 app = Flask(__name__)
@@ -8,7 +9,8 @@ app = Flask(__name__)
 key = os.urandom(21) 
 app.secret_key = key # This will be a secret key to hold user session. 
 
-
+#Make a dictionary Key, user ID : value, password
+is_pwd: Dict[str, str] = {"lelouch" : "vermillioin"}
 
 #Assuming it is a "main" function
 # "/" indicates URL path
@@ -17,6 +19,9 @@ app.secret_key = key # This will be a secret key to hold user session.
 def index() -> str:
 	return "Hello World"
 
+@app.route("/login")
+def login():
+	return render_template('login.html')
 
 #Launch this app
 if __name__ == "__main__":

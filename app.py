bafa782f272a87f8d1b377b601dec35f07a537dc
KEITA - KATSUMI <keita-katsumi@KEITAs-MBP.lan>
2024-08-29 08:19:14 -0700

    Implementing logincheck() in app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, session, request, redirect, url_for
+from flask import Flask, render_template, session, request, redirect, url_for, Response
 import os # It is used for making tokens. 
-from typing import Any, Dict
+from typing import Any, Dict, List, Tuple, Optional, Union, # This is for type hints
+# Union: Allows you to specify multiple types for a variable. 
+# For example, Union[int, str] means the variable can be an integer or a string.
+
 
 #Make a Flask instance
 app = Flask(__name__)
@@ -19,9 +22,32 @@ is_pwd: Dict[str, str] = {"lelouch" : "vermillioin"}
 def index() -> str:
 	return "Hello World"
 
-@app.route("/login")
-def login():
+@app.route("/login") 
+def login() -> str:
+	# Supposed to return the rendered HTML template as a string
 	return render_template('login.html')
+
+# Login authentication
+ # The function returns either string (a URL) or a Flask"Response" object
+@app.route("/logincheck", methods=["POST"])
+def logincheck() -> Union[str, "Response"]: 
+	user_id: str = request.form["user_id", ""]
+	password: str  = request.form["password", ""]
+
+	if user_id in id_pwd and password == id_pwd.get(user_id):
+			session["login"] = True
+	else:
+		session["login"] = False
+
+	# The url_for function takes the name of a view function 
+	#(i.e., the function that is decorated with @app.route) and 
+	# returns the corresponding URL for that function. 
+	if session["login"]:
+		return redirect(url_for("index"))
+	else:
+		return redirect(url_for("login"))
+
+
 
 #Launch this app
 if __name__ == "__main__":
